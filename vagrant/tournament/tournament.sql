49085fa63cb34fe58cce104880bacd12c18fba3b
Ivo <Ivo@mongtekiMacBook-Air.local>
2015-09-25 10:58:36 +0800

    adding comments to tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -4,16 +4,29 @@
 -- statements if you choose to use it.
 
 
+-- initiating database, it will throw a non-critical error if it already exists
 CREATE DATABASE tournament;
-	DROP VIEW IF EXISTS winners;
-	DROP TABLE IF EXISTS matches;
-	DROP TABLE IF EXISTS players;
 
-	CREATE TABLE players (id serial primary key,
-							name text);
 
-	CREATE TABLE matches (id serial primary key,
-							winner serial references players(id),
-							loser serial references players(id),
-							time timestamptz DEFAULT CURRENT_TIMESTAMP);
+-- dropping existing tables in the order of dependencies. All data will be lost
+-- it must be removed from production and upgrades
+DROP VIEW IF EXISTS winners;
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS players;
+
+
+-- creating table players. Currently only full name in one string and 
+-- ID as serial (primary key) are needed. more relevant info about player might 
+-- be added in the future
+CREATE TABLE players (id serial primary key,
+						name text);
+
+
+-- creating table matches, which records, which two players played and how is 
+-- winner and loser. ID column is added because two players might meet each other 
+-- repeatedly. time stamp is added for potential future use.
+CREATE TABLE matches (id serial primary key,
+						winner integer references players(id),
+						loser integer references players(id),
+						time timestamptz DEFAULT CURRENT_TIMESTAMP);
 
